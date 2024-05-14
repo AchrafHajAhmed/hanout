57bb721c5e93cfc673284d4ca66e8fade4574e79
Achraf <achrafhajahmed12@gmail.com>
2024-05-14 03:46:03 +0100

    fixation des erreur

--- lib/screen/services/auth.dart
+++ lib/screen/services/auth.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -9,6 +8,14 @@ class Auth {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
+  Future<UserCredential> signInWithEmailAndPassword(String email, String password) async {
+    return await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<UserCredential> createUserWithEmailAndPassword(String email, String password) async {
+    return await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+  }
+
   Future<void> addUserToFirestore(String userId, String name, String email) async {
     await _firestore.collection('users').doc(userId).set({
       'uid': userId,
@@ -16,34 +23,30 @@ class Auth {
       'email': email,
     });
   }
-  Future<void> addImagesToMerchant(String userId, String idCardName, String fiscalCardName) async {
-    await _firestore.collection('commercants').doc(userId).update({
-      'idCardName': idCardName,
-      'fiscalCardName': fiscalCardName,
-    });
-  }
-
-  Future<UserCredential> signInWithEmailAndPassword(String email, String password) async {
-    return await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-  }
 
   Future<UserCredential?> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-    if (googleUser != null) {
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-      final UserCredential userCredential = await _firebaseAuth.signInWithCredential(credential);
-      await addUserToFirestore(
-        userCredential.user!.uid,
-        userCredential.user!.displayName ?? '',
-        userCredential.user!.email ?? '',
-      );
-      return userCredential;
+    try {
+      final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+        final UserCredential userCredential = await _firebaseAuth.signInWithCredential(credential);
+
+        await addUserToFirestore(
+          userCredential.user!.uid,
+          userCredential.user!.displayName ?? '',
+          userCredential.user!.email ?? '',
+        );
+
+        return userCredential;
+      }
+    } catch (error) {
+      print(error.toString());
+      return null;
     }
-    return null;
   }
 
   Future<void> signOutGoogle() async {
@@ -52,67 +55,39 @@ class Auth {
   }
 
   Future<UserCredential?> signInWithFacebook() async {
-    final LoginResult loginResult = await FacebookAuth.instance.login();
-    if (loginResult.status == LoginStatus.success) {
-      final AccessToken accessToken = loginResult.accessToken!;
-      final AuthCredential credential = FacebookAuthProvider.credential(accessToken.token);
-      final UserCredential userCredential = await _firebaseAuth.signInWithCredential(credential);
-      await addUserToFirestore(
-        userCredential.user!.uid,
-        userCredential.user!.displayName ?? 'Facebook User',
-        userCredential.user!.email ?? '',
-      );
-      return userCredential;
+    try {
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+      if (loginResult.status == LoginStatus.success) {
+        final AccessToken accessToken = loginResult.accessToken!;
+        final AuthCredential facebookCredential = FacebookAuthProvider.credential(accessToken.token);
+        final UserCredential userCredential = await _firebaseAuth.signInWithCredential(facebookCredential);
+
+        await addUserToFirestore(
+          userCredential.user!.uid,
+          userCredential.user!.displayName ?? 'Facebook User',
+          userCredential.user!.email ?? '',
+        );
+
+        return userCredential;
+      } else {
+        print("Erreur de connexion Facebook: ${loginResult.status}");
+        return null;
+      }
+    } catch (error) {
+      print("Erreur d'authentification Facebook: $error");
+      return null;
     }
-    return null;
   }
 
   Future<void> signOutFacebook() async {
     await FacebookAuth.instance.logOut();
     await _firebaseAuth.signOut();
   }
-
   Future<void> sendPasswordResetEmail(String email) async {
-    await _firebaseAuth.sendPasswordResetEmail(email: email);
-  }
-
-  Future<UserCredential> createUserWithEmailAndPassword(String email, String password) async {
     try {
-      final UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      await addUserToFirestore(userCredential.user!.uid, '', email);
-      return userCredential;
-    } catch (e) {
-      throw Exception('Failed to create user: $e');
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      throw Exception('Erreur Firebase: ${e.message}');
     }
   }
-
-  Future<UserCredential> createMerchant(
-      String name,
-      String email,
-      String phoneNumber,
-      String fiscalNumber,
-      String password,
-      // Remove the File parameters here
-      ) async {
-    UserCredential userCredential = await createUserWithEmailAndPassword(email, password);
-    try {
-      // You can handle the merchant creation logic here without image upload
-      await _firestore.collection('commercants').doc(userCredential.user!.uid).set({
-        'uid': userCredential.user!.uid,
-        'name': name,
-        'email': email,
-        'phone': phoneNumber,
-        'fiscalNumber': fiscalNumber,
-        'status': false, // Ajouter le champ "status" avec la valeur par défaut "false"
-      });
-      return userCredential;
-    } catch (e) {
-      await userCredential.user!.delete(); // Rollback user creation if subsequent steps fail
-      throw Exception('Failed to create merchant: $e');
-    }
-  }
-
 }
